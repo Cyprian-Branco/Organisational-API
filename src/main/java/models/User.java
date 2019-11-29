272d0d8a91effe1a0820da64e4a266a9d688cfae
Cyprian Bowen <bowencyprian@gmail.com>
2019-11-29 17:42:18 +0300

    initialize the variables

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -1,4 +1,16 @@
 package models;
 
 public class User {
+    public String username;
+    private String company_position;
+    private String role;
+    private int id;
+    private int departmentId;
+
+    public User(String username, String company_position, String role, int departmentId){
+        this.username = username;
+        this.company_position = company_position;
+        this.role =role;
+        this.departmentId = departmentId;
+    }
 }
