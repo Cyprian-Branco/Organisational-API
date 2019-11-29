962f04191ee102f2495ca327aa3af536102a85ab
Cyprian Bowen <bowencyprian@gmail.com>
2019-11-29 16:46:59 +0300

    create news setter and getter methods

--- src/main/java/models/News.java
+++ src/main/java/models/News.java
@@ -1,4 +1,22 @@
 package models;
 
 public class News {
+    public String content;
+    public String type;
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setContent(String content){
+        this.content = content;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 }
