a69540293f5de0102609b67ce035f87d03fa0981
Cyprian Bowen <bowencyprian@gmail.com>
2019-11-29 19:11:15 +0300

    test getUsername getter instantiates correctly

--- src/test/java/models/UserTest.java
+++ src/test/java/models/UserTest.java
@@ -39,6 +39,8 @@ public class UserTest {
 
     @Test
     public void getUsernameInstantiatesCorrectly() {
+        User testUser = setupUser();
+        assertEquals("mike", testUser.getUsername());
     }
 
     @Test
