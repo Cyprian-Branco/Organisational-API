f9ae7f45d37c18c434af878601249be08aef071a
Cyprian Bowen <bowencyprian@gmail.com>
2019-11-29 17:09:33 +0300

    initialize departmenrNews class variables

--- src/main/java/models/DepartmentNews.java
+++ src/main/java/models/DepartmentNews.java
@@ -1,4 +1,13 @@
 package models;
 
-public class DepartmentNews {
+public class DepartmentNews  extends News{
+    public static final String DATABASE_TYPE = "department";
+    private int departmentId;
+    private int id;
+
+    public DepartmentNews(String content, int departmentId){
+        this.content = content;
+        this.departmentId =departmentId;
+        this.type = DATABASE_TYPE;
+    }
 }
