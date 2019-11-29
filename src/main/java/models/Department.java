22a6d9341ed0ad021ee170bfbfe940b281940818
Cyprian Bowen <bowencyprian@gmail.com>
2019-11-29 15:44:48 +0300

    initialize department variables

--- src/main/java/models/Department.java
+++ src/main/java/models/Department.java
@@ -1,4 +1,14 @@
 package models;
 
 public class Department {
+    private String DeptName;
+    private String description;
+    private int NoOfEmployees;
+    private int id;
+
+    public Department(String deptName, String description, int NoOfEmployees){
+        this.DeptName = deptName;
+        this.description = description;
+        this.NoOfEmployees = NoOfEmployees;
+    }
 }
