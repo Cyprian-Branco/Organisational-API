6226961c3358e4a7f107d848656d0559d34df096
Cyprian Bowen <bowencyprian@gmail.com>
2019-11-29 17:36:57 +0300

    initialize the variables

--- src/main/java/models/GeneralNews.java
+++ src/main/java/models/GeneralNews.java
@@ -1,4 +1,12 @@
 package models;
 
-public class GeneralNews {
+public class GeneralNews extends News{
+    public static final String DATABASE_TYPE = "general";
+    private int id;
+
+    public GeneralNews(String content){
+        this.content = content;
+        this.type = DATABASE_TYPE;
+    }
+
 }
