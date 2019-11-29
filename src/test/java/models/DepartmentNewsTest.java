862877136d64523b31e11025adeb2ac9cf068d15
Cyprian Bowen <bowencyprian@gmail.com>
2019-11-29 19:37:16 +0300

    create test files

---
+++ src/test/java/models/DepartmentNewsTest.java
@@ -0,0 +1,20 @@
+package models;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DepartmentNewsTest {
+
+    @Test
+    public void getDepartmentId() {
+    }
+
+    @Test
+    public void getId() {
+    }
+
+    @Test
+    public void setDepartmentId() {
+    }
+}
---
+++ src/test/java/models/GeneralNewsTest.java
@@ -0,0 +1,16 @@
+package models;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GeneralNewsTest {
+
+    @Test
+    public void getId() {
+    }
+
+    @Test
+    public void setId() {
+    }
+}
