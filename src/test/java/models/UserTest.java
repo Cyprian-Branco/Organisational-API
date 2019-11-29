e1c8bccb06c370bc26c865ac8f28f5f7e5281054
Cyprian Bowen <bowencyprian@gmail.com>
2019-11-29 19:07:33 +0300

    test getDepartmenid getter instantiates correctly

---
+++ src/test/java/models/UserTest.java
@@ -0,0 +1,57 @@
+package models;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UserTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+    public User setupUser(){
+        return new User("mike", "manager", "supervise", 3);
+    }
+
+    @Test
+    public void getDepartmentIdInstantiatesCorrectly(){
+        User testUser = setupUser();
+        assertEquals(3, testUser.getDepartmentId());
+    }
+
+    
+
+    @Test
+    public void getUsernameInstantiatesCorrectly() {
+    }
+
+    @Test
+    public void getId() {
+    }
+
+    @Test
+    public void setUsername() {
+    }
+
+    @Test
+    public void setCompany_position() {
+    }
+
+    @Test
+    public void setRole() {
+    }
+
+    @Test
+    public void setId() {
+    }
+
+    @Test
+    public void setDepartmentId() {
+    }
+}
