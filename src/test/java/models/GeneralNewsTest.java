2687f160a1012532908bc4f65ddb2cfd49207b51
Cyprian Bowen <bowencyprian@gmail.com>
2019-11-30 14:36:54 +0300

    test set and get methods

--- src/test/java/models/GeneralNewsTest.java
+++ src/test/java/models/GeneralNewsTest.java
@@ -1,16 +1,43 @@
 package models;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class GeneralNewsTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+    public GeneralNews setupGeneralNews(){
+        return new GeneralNews("news");
+
+    }
+    @Test
+    public void setContentInstantiatesCorrectly(){
+        GeneralNews testGeneralNews = setupGeneralNews();
+        testGeneralNews.setContent("news");
+        assertEquals("news", GeneralNews.getContent());
+    }
 
     @Test
-    public void getId() {
+    public void getIdInstantiatesCorrectly() {
+        GeneralNews testGeneralNews = setupGeneralNews();
+        assertEquals(0, testGeneralNews.getId());
     }
 
     @Test
     public void setId() {
+    }
+
+    @Test
+    public void name() {
     }
 }
