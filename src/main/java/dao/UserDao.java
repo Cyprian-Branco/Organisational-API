639f7415028b03c658a9ad475e35279df58dca01
Cyprian Bowen <bowencyprian@gmail.com>
2019-12-01 21:48:13 +0300

    fix bugs

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 public interface UserDao {
     void add(User user);
+
     List<User>getAll();
     List<User> getAllUsersByDepartment(int departmentId);
     void deleteById(int id);
