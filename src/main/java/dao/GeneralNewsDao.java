1daac690e04373310ace84e157eccb503a839925
Cyprian Bowen <bowencyprian@gmail.com>
2019-12-01 20:16:56 +0300

    add details into dao

--- src/main/java/dao/GeneralNewsDao.java
+++ src/main/java/dao/GeneralNewsDao.java
@@ -1,4 +1,14 @@
 package dao;
 
+import models.Department;
+
+import java.util.List;
+
 public interface GeneralNewsDao {
+
+    void add (Department department);
+
+    List<Department> getAll();
+    Department findById(int id);
+
 }
