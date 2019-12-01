e23a5aa773d7dea17d697280301c0bfec900f895
Cyprian Bowen <bowencyprian@gmail.com>
2019-12-01 19:43:08 +0300

    fix errord

--- src/test/java/models/DepartmentNewsTest.java
+++ src/test/java/models/DepartmentNewsTest.java
@@ -5,16 +5,14 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class DepartmentNewsTest {
-
-    @Test
-    public void getDepartmentId() {
+    public DepartmentNews setupDepartmentNews() {
+        return new DepartmentNews("agricultural tools", 5);
     }
 
     @Test
-    public void getId() {
+    public void getDepartmentIdInstantiatesCorrectly() {
+        DepartmentNews testDepartmentNews = setupDepartmentNews();
+        assertEquals(5, testDepartmentNews.getDepartmentId());
     }
 
-    @Test
-    public void setDepartmentId() {
-    }
 }
